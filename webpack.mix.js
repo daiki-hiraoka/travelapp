c3b8efe4d3731ce12b627f8edfc24ba3db986414
rskb0126 <dai.0120.ki@gmail.com>
2023-01-23 22:51:30 +0900

    圧縮処理途中

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,19 +13,19 @@ const CompressionPlugin = require('compression-webpack-plugin');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css').vue()
+   .sass('resources/sass/app.scss', 'public/css')
    // app.js作成時にgzファイルを作成する
-  .webpackConfig({
-      plugins: [
-          new CompressionPlugin({
-              filename: '[path].gz[query]',
-              algorithm: 'gzip',
-              test: /\.js$|\.css$|\.html$|\.svg$/,
-              threshold: 10240,
-              minRatio: 0.8,
-      })
-    ]
-  });
+   .webpackConfig({
+       plugins: [
+           new CompressionPlugin({
+               filename: '[path].gz[query]',
+               algorithm: 'gzip',
+               test: /\.js$|\.css$|\.html$|\.svg$/,
+               threshold: 10240,
+               minRatio: 0.8,
+           })
+        ]
+   }).vue();
 
 if (mix.inProduction()) {                                                                                                                                                                                          
     mix.version();                                                                                                                                                                                                 
