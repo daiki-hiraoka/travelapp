a4eed6fed5a2b65b7d6bd0de087c6f512dcdb664
rskb0126 <dai.0120.ki@gmail.com>
2023-01-23 23:34:58 +0900

    webpack.mix.jsでgzipファイルの出力

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,5 @@
 const mix = require('laravel-mix');
-const CompressionPlugin = require('compression-webpack-plugin');
-
+const CompressionPlugin = require('compression-webpack-plugin');//追加
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -12,21 +11,54 @@ const CompressionPlugin = require('compression-webpack-plugin');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css')
-   // app.js作成時にgzファイルを作成する
-   .webpackConfig({
-       plugins: [
-           new CompressionPlugin({
-               filename: '[path].gz[query]',
-               algorithm: 'gzip',
-               test: /\.js$|\.css$|\.html$|\.svg$/,
-               threshold: 10240,
-               minRatio: 0.8,
-           })
-        ]
-   }).vue();
 
-if (mix.inProduction()) {                                                                                                                                                                                          
-    mix.version();                                                                                                                                                                                                 
-}    
+mix.js('resources/js/app.js', 'public/js').vue()
+   .sass('resources/sass/app.scss', 'public/css')
+   //追加
+   .webpackConfig({
+    plugins: [
+      new CompressionPlugin({
+        // filename: '[path].gz[query]',
+        // algorithm: 'gzip',
+        test: /\.js$|\.css$|\.html$|\.svg$/,
+        // threshold: 10240,
+        // minRatio: 0.8,
+      })
+    ]
+  });
+
+if (mix.inProduction()) {
+    mix.version();
+}
+// const mix = require('laravel-mix');
+// const CompressionPlugin = require('compression-webpack-plugin');
+
+// /*
+//  |--------------------------------------------------------------------------
+//  | Mix Asset Management
+//  |--------------------------------------------------------------------------
+//  |
+//  | Mix provides a clean, fluent API for defining some Webpack build steps
+//  | for your Laravel application. By default, we are compiling the Sass
+//  | file for the application as well as bundling up all the JS files.
+//  |
+//  */
+
+// mix.js('resources/js/app.js', 'public/js').vue()
+//   .sass('resources/sass/app.scss', 'public/css')
+//   // app.js作成時にgzファイルを作成する
+//   .webpackConfig({
+//       plugins: [
+//           new CompressionPlugin({
+//               filename: '[path].gz[query]',
+//               algorithm: 'gzip',
+//               test: /\.js$|\.css$|\.html$|\.svg$/,
+//               threshold: 10240,
+//               minRatio: 0.8,
+//           })
+//         ]
+//   });
+
+// if (mix.inProduction()) {                                                                                                                                                                                          
+//     mix.version();                                                                                                                                                                                                 
+// }    
